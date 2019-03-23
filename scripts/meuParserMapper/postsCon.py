b33680978a723908e312121118bdfd70cb172c12
ramalhohugo <ramalho.hg@gmail.com>
2019-03-23 20:03:49 -0300

    Criar classe de conexão com posts

--- scripts/meuParserMapper/postsCon.py
+++ scripts/meuParserMapper/postsCon.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 class pgConnection:
-    def __init__(self, user="" ,password="", host="127.0.0.1",port="5432", database=""):
+    def __init__(self, user="postgres" ,password="123456", host="127.0.0.1",port="5432", database="Trabalho1"):
         self.user = user
         self.password = password
         self.host = host
@@ -14,11 +14,11 @@ class pgConnection:
     def connect(self):
         if self.user is not '':
             try:
-                self.connection = psycopg2.connect(user = "sysadmin",
-                                              password = "pynative@#29",
-                                              host = "127.0.0.1",
-                                              port = "5432",
-                                              database = "postgres_db")
+                self.connection = psycopg2.connect(user = self.user,
+                                              password = self.password,
+                                              host = self.host,
+                                              port = self.port,
+                                              database = self.database)
                 self.cursor = self.connection.cursor()
                 # Print PostgreSQL Connection properties
                 print ( self.connection.get_dsn_parameters(),"\n")
@@ -40,36 +40,8 @@ class pgConnection:
 
 
 
+def main():
+    conn = pgConnection()
+    conn.connect()
 
-
-class Conexao(object):
-    _db=None
-    def __init__(self, mhost, db, usr, pwd):
-        self._db = psycopg2.connect(host=mhost, database=db, user=usr,  password=pwd)
-    def manipular(self, sql):
-        try:
-            cur=self._db.cursor()
-            cur.execute(sql)
-            cur.close()
-            self._db.commit()
-        except:
-            return False
-        return True
-    def consultar(self, sql):
-     rs=None
-     try:
-         cur=self._db.cursor()
-         cur.execute(sql)
-         rs=cur.fetchall()
-     except:
-         return None
-     return rs
-
-    def proximaPK(self, tabela, chave):
-        sql='select max('+chave+') from '+tabela
-        rs = self.consultar(sql)
-        pk = rs[0][0]
-        return pk+1
-
-    def fechar(self):
-        self._db.close()
+main()
